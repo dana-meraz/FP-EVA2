cb5fa53cfb2d921dce27e83ceecec3d258026a7a
dana-meraz <L24550372@chihuahua2.tecnm.mx>
2024-10-03 16:29:35 -0600

    Add files via upload

---
+++ EVA2_1_CICLOS.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_1_ciclos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_1_CICLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //REPETIR DE INSTRUCCIONES
+        //CICLOS O BUCLES
+        //FOR (DESDE), WHILE (MIENTRAS), DO WHILE (HACER MIENTRAS)
+        //FOR TIENE 3 PARAMETROS, PUNTO DE INICIO, condicio
+        //CICLO FOR
+        
+        
+        //Alcance: El lugar donde un edentificador bloque de codigo {}
+        
+        
+        for(int i = 1; i <= 10; i++){
+           //++ --> operador para sumar 1 unidad
+            //-- --> resta una unida 
+            //+= suma N valor
+            //-= resta N valor
+            //*= Multiprica por N
+            // /= Divide entre N
+            System.out.println(i + " - ");
+        }
+        System.out.println("");
+        for(int i = 10; i >= 1; i--){//La varieble i solo existe dentro del for
+     System.out.println(i + " - ");
+    }
+    }
+    
+}
