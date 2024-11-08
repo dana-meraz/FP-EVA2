ce8e0fb9090f913df4bb475b6cbd659d6b64df32
dana-meraz <L24550372@chihuahua2.tecnm.mx>
2024-11-08 16:53:00 -0600

    Add files via upload

---
+++ Examen_practico_eva2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package examen_practico_eva2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class Examen_practico_eva2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner captu = new Scanner(System.in);
+        int num;
+        System.out.println("Escoje un numero 1-piedra, 2-tijeras, 3-papel");
+        num = captu.nextInt();
+        captu.nextLine();
+        //math.random()
+        
+        case 1:
+        System.out.plintln("piedra");
+        break;
+        case 2:
+        System.out.println("tijeras");
+        break;
+        case 3:
+        System.out.println("papel");
+        break;
+    
+        
+    }
+    
+}
